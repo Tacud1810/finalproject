731494fd824353cacddba41970c35f3276c54850
Tacud1810 <r.kachyna@gmail.com>
2023-07-15 07:36:28 +0200

    login and signup. Accounts app.

--- library/settings.py
+++ library/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
 
     'viewer',
+    'accounts',
 
 ]
 
@@ -144,3 +145,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
