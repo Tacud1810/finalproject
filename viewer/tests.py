96bcafd44e3a9015e2c7f556f9694701a4ea1885
Tacud1810 <r.kachyna@gmail.com>
2023-07-28 18:27:11 +0200

    final version v.2.0.0

--- viewer/tests.py
+++ viewer/tests.py
@@ -18,51 +18,52 @@ class GenreFromTest(LiveServerTestCase):
 		book = driver.find_element(By.ID, 'new-book')
 		book.click()
 		time.sleep(3)
-	#
-	def test_sign_up(self):
-		driver = webdriver.Chrome()
 
-		driver.get("http://127.0.0.1:8000/")
-		time.sleep(2)
-		login = driver.find_element(By.ID, 'sign-up-btn')
-		login.click()
-		time.sleep(2)
-		form_username = driver.find_element(By.ID, 'id_username')
-		form_first_name = driver.find_element(By.ID, 'id_first_name')
-		form_last_name = driver.find_element(By.ID, 'id_last_name')
-		form_email = driver.find_element(By.ID, 'id_email')
-		form_password1 = driver.find_element(By.ID, 'id_password1')
-		form_password2 = driver.find_element(By.ID, 'id_password2')
-		form_username.send_keys('tmalinky')
-		form_first_name.send_keys('Tomáš')
-		form_last_name.send_keys('Malý')
-		form_email.send_keys('malinkyt@gmail.com')
-		form_password1.send_keys('pass123456')
-		form_password2.send_keys('pass123456')
-		time.sleep(2)
-		form_submit = driver.find_element(By.ID, 'btn-submit')
-		form_submit.click()
-		time.sleep(2)
 	#
-	# 	login = driver.find_element(By.ID, 'login-id')
-	# 	login.click()
-	# 	time.sleep(2)
-	# 	form_username = driver.find_element(By.ID, 'id_username')
-	# 	form_password = driver.find_element(By.ID, 'id_password')
-	# 	form_username.send_keys('tmalinky')
-	# 	form_password.send_keys('pass123456')
-	# 	time.sleep(2)
-	# 	form_submit = driver.find_element(By.ID, 'btn-submit')
-	# 	form_submit.click()
-	# 	time.sleep(2)
-	#
-	# 	book = driver.find_element(By.ID, 'new-book')
-	# 	book.click()
-	# 	time.sleep(3)
-	#
-	# 	logout = driver.find_element(By.ID, 'logout-btn')
-	# 	logout.click()
-	# 	time.sleep(2)
+# 	def test_sign_up(self):
+# 		driver = webdriver.Chrome()
+#
+# 		driver.get("http://127.0.0.1:8000/")
+# 		# time.sleep(2)
+# 		login = driver.find_element(By.ID, 'sign-up-btn')
+# 		login.click()
+# 		# time.sleep(2)
+# 		form_username = driver.find_element(By.ID, 'id_username')
+# 		form_first_name = driver.find_element(By.ID, 'id_first_name')
+# 		form_last_name = driver.find_element(By.ID, 'id_last_name')
+# 		form_email = driver.find_element(By.ID, 'id_email')
+# 		form_password1 = driver.find_element(By.ID, 'id_password1')
+# 		form_password2 = driver.find_element(By.ID, 'id_password2')
+# 		form_username.send_keys('tmalinky')
+# 		form_first_name.send_keys('Tomáš')
+# 		form_last_name.send_keys('Malý')
+# 		form_email.send_keys('malinkyt@gmail.com')
+# 		form_password1.send_keys('pass123456')
+# 		form_password2.send_keys('pass123456')
+# 		# time.sleep(2)
+# 		form_submit = driver.find_element(By.ID, 'btn-submit')
+# 		form_submit.click()
+# 		# time.sleep(2)
+# #
+# 		login = driver.find_element(By.ID, 'login-id')
+# 		login.click()
+# 		# time.sleep(2)
+# 		form_username = driver.find_element(By.ID, 'id_username')
+# 		form_password = driver.find_element(By.ID, 'id_password')
+# 		form_username.send_keys('tmalinky')
+# 		form_password.send_keys('pass123456')
+# 		# time.sleep(2)
+# 		form_submit = driver.find_element(By.ID, 'btn-submit')
+# 		form_submit.click()
+# 		# time.sleep(2)
+#
+# 		book = driver.find_element(By.ID, 'new-book')
+# 		book.click()
+# 		# time.sleep(3)
+#
+# 		logout = driver.find_element(By.ID, 'logout-btn')
+# 		logout.click()
+# 		# time.sleep(2)
 
 	# def test_login(self):
 	# 	driver = webdriver.Chrome()
@@ -86,14 +87,14 @@ class GenreFromTest(LiveServerTestCase):
 	# 	# time.sleep(2)
 	# 	user = driver.find_element(By.ID, 'user-page')
 	# 	user.click()
-	# 	# time.sleep(2)
+	# 	time.sleep(2)
 	# 	user = driver.find_element(By.ID, 'extend-btn')
 	# 	user.click()
 	# 	time.sleep(4)
 	# 	try:
-	# 		alert = driver.switch_to.alert
-	# 		print(f"alert: {alert}")
-	# 		alert.accept()
+	# 		driver.switchTo.alert().accept()
+	# 		# print(f"alert: {alert}")
+	# 		# alert.accept()
 	# 	except:
 	# 		pass
 	# 	logout = driver.find_element(By.ID, 'logout-btn')
@@ -129,18 +130,14 @@ class GenreFromTest(LiveServerTestCase):
 	# 	cart.click()
 	# 	time.sleep(2)
 	#
-	# 	order = driver.find_element(By.LINK_TEXT, 'Finish order')
+	# 	order = driver.find_element(By.ID, 'finish-button')
 	# 	order.click()
 	# 	time.sleep(2)
 	#
-	# 	orders = driver.find_element(By.LINK_TEXT, 'Orders')
+	# 	orders = driver.find_element(By.ID, 'orders')
 	# 	orders.click()
 	# 	time.sleep(2)
 	#
 	# 	logout = driver.find_element(By.ID, 'logout-btn')
 	# 	logout.click()
 	# 	time.sleep(2)
-
-
-
-
